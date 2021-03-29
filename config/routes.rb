ba8b84306d3e3c85146e751e60a62a7e482f982e
Adriana <adriana.amaral@msn.com>
2021-03-28 19:44:46 -0500

    commit tree eaa9e200a5459b57917b8cd11201ea51407cec66

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # get 'products/index'
+
+  # get 'products/add'
+
+
+  # get 'products#index', as: :products
+
+  # post  'products#add', as: :add_product
+  get '/', to: 'products#index', as: :products
+
+  post '/add', to: 'products#add', as: :add_product
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
